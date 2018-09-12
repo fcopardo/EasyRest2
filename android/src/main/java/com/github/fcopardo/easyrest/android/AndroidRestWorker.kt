2031493bd8eb4c26dd554934ff360f9c5795c7f8
Fco Pardo <Francisco.pardo.baeza@gmail.com>
2018-09-12 18:09:22 -0300

    WIP

--- android/src/main/java/com/github/fcopardo/easyrest/android/AndroidRestWorker.kt
+++ android/src/main/java/com/github/fcopardo/easyrest/android/AndroidRestWorker.kt
@@ -2,5 +2,8 @@ package com.github.fcopardo.easyrest.android
 
 import com.github.fcopardo.easyrest.common.BaseJVMRestWorker
 
-class AndroidRestWorker<T, X> : BaseJVMRestWorker<T, X>() {
+class AndroidRestWorker<T, X> : BaseJVMRestWorker<T, X> {
+
+    constructor():super()
+
 }
